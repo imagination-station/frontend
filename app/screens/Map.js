ef6b9f20a126a6eb2d32af1a5c31aea4b1549188
Youn <ybae35@gatech.edu>
2019-10-02 18:56:41 -0400

    added search bar to map

--- app/screens/Map.js
+++ app/screens/Map.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
-import MapView from 'react-native-maps';
+import { View, TextInput, StyleSheet } from 'react-native';
+import MapView, { Callout } from 'react-native-maps';
+
+const styles = StyleSheet.create({
+  calloutView: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  calloutSearch: {
+    backgroundColor: 'rgba(255, 255, 255, 0.9)',
+    height: 40,
+    borderRadius: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
+    width: '70%',
+    top: 20
+  }
+})
 
 class MapScreen extends Component {
+  state = {
+    search: ''
+  };
+
   render() {
-    return <MapView style={{flex: 1}}/>;
+    return (
+      <View style={{flex: 1}}>
+        <MapView style={{flex: 1}} />
+        <Callout style={{width: '100%'}}>
+          <View style={styles.calloutView}>
+            <TextInput
+              style={styles.calloutSearch}
+              placeholder={'Search'}
+            />
+          </View>
+        </Callout>
+      </View>
+    );
   }
 }
 
