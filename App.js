09eb4c6b22435dd545998c2c7aa47c9574fcb712
Younmin Bae <ybae35@gatech.edu>
2019-10-07 01:28:29 -0400

    initialized firebase

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import * as firebase from 'firebase';
 
 import LoginScreen from './app/screens/Login.js';
 import HomeScreen from './app/screens/Home.js';
+import ProfileScreen from './app/screens/Profile.js';
 import MapScreen from './app/screens/Map.js';
+
+import { FIREBASE_CONFIG } from './app/config/settings.js';
+
+firebase.initializeApp(FIREBASE_CONFIG);
 
 const AppNavigator = createStackNavigator(
   {
@@ -14,12 +20,15 @@ const AppNavigator = createStackNavigator(
     Home: {
       screen: HomeScreen
     },
+    Profile: {
+      screen: ProfileScreen
+    },
     Map: {
       screen: MapScreen
     }
   },
   {
-    initialRouteName: 'Map'
+    initialRouteName: 'Login'
   }
 );
 
