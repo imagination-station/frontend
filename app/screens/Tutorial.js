44d49e5f418803d1958b8ed3efa2f034d0dd1182
Rahul Zhade <rzhade3@users.noreply.github.com>
2019-11-22 15:42:59 -0500

    Finished copy for tutorial screens

--- app/screens/Tutorial.js
+++ app/screens/Tutorial.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
-import { BackHandler, Dimensions, View, StyleSheet, Text, StatusBar, TouchableOpacity, SafeAreaView, ScrollView, Platform } from 'react-native';
+import { BackHandler, Dimensions, Image, View, StyleSheet, Text, StatusBar, SafeAreaView, ScrollView, Platform } from 'react-native';
 import { Header } from 'react-navigation-stack';
-import { DARKER_GREY, GREY, PRIMARY } from '../config/styles.js';
+import { DARKER_GREY, GREY, PRIMARY, ACCENT } from '../config/styles.js';
 
 const screens = {
   MYTRIPS: 'm',
@@ -15,24 +15,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    backgroundColor: 'white',
     paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight
   },
+  imageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   mainText: {
-    fontSize: 20,
-    fontWeight: 'bold'
+    fontSize: 40,
+    fontWeight: 'bold',
+    textAlign: 'center'
   },
   screen: {
     flex: 1,
     padding: 10,
-    width: width
-  },
-  safeArea: {
-    flex: 1,
-    backgroundColor: GREY
+    width: width,
+    backgroundColor: 'white',
   },
   scrollView: {
     flex: 1
+  },
+  text: {
+    fontSize: 15
+  },
+  tutScreen: {
+    height: 400,
+    resizeMode: 'contain',
+    marginTop: 50,
+    marginBottom: 50
   }
 });
 
@@ -40,7 +50,7 @@ class TutorialScreen extends Component {
 
   static navigationOptions = {
     tabBarVisible: false,
-    headerTitle: () => <Text style={{fontSize: 20}}>Tutorial</Text>
+    headerTitle: () => <Text style={{fontSize: 20}}>Tutorial</Text>,
   };
 
   constructor(props) {
@@ -60,44 +70,57 @@ class TutorialScreen extends Component {
         <ScrollView style={styles.scrollView} horizontal={true} pagingEnabled={true}>
           <View style={styles.screen}>
             <Text style={styles.mainText}>
-              Yo
+              Stumble
             </Text>
+            <SafeAreaView style={styles.imageContainer}>
+              <Image source={require('../assets/screens/splash.jpg')} style={styles.tutScreen}>
+              </Image>
+            </SafeAreaView>
             <Text style={styles.text}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.
+              Welcome to Stumble! {"\n"} {"\n"}
+              In this app, you can view authentic trips and experiences within a city in order to help make your next trip a little easier.
             </Text>
           </View>
           <View style={styles.screen}>
             <Text style={styles.mainText}>
-              Sup
+              View a Route
             </Text>
+            <SafeAreaView style={styles.imageContainer}>
+              <Image source={require('../assets/screens/view-route.jpg')} style={styles.tutScreen}>
+              </Image>
+            </SafeAreaView>
             <Text style={styles.text}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.
+              View routes that other users have created {"\n"} {"\n"}
+              • Search for a route by distance, time or interest, or select one from your personalized feed {"\n"}
+              • View characteristics of the route by scrolling to the end of points {"\n"}
+              • Find routes between two points by clicking the path icon {"\n"}
             </Text>
           </View>
           <View style={styles.screen}>
             <Text style={styles.mainText}>
-              Bro
+              Create a Route
             </Text>
+            <SafeAreaView style={styles.imageContainer}>
+              <Image source={require('../assets/screens/explore.jpg')} style={styles.tutScreen}>
+              </Image>
+            </SafeAreaView>
             <Text style={styles.text}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.
+              Create your own route by clicking the plus icon in the search bar. {"\n"} {"\n"}
+              • Select your list of points in order {"\n"}
+              • Select the amount of time to spend at each point {"\n"}
+              • Name your route and tag it {"\n"}
+            </Text>
+          </View>
+          <View style={styles.screen}>
+            <Text style={styles.mainText}>
+              Edit a Route
+            </Text>
+            <SafeAreaView style={styles.imageContainer}>
+              <Image source={require('../assets/screens/edit-route.jpg')} style={styles.tutScreen}>
+              </Image>
+            </SafeAreaView>
+            <Text style={styles.text}>
+              Editing an existing route is not currently available, but will be soon! Stay tuned for more.
             </Text>
           </View>
         </ScrollView>
